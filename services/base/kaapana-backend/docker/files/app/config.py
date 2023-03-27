69af3486f690cbb9d94133d1c8ce665d8ae6a27f
hanno <h.gao@dkfz-heidelberg.de>
2023-03-27 18:22:51 +0200

    m2olie in kaapana-backend for now -> needs to get own backend

--- services/base/kaapana-backend/docker/files/app/config.py
+++ services/base/kaapana-backend/docker/files/app/config.py
@@ -40,6 +40,8 @@ class Settings(BaseSettings):
 
     airflow_url: str = os.getenv("AIRFLOW_URL")
     services_namespace: str = os.getenv("SERVICES_NAMESPACE")
+    dcm4chee_url: str = os.getenv("DCM4CHEE_URL")
+
 
 
 settings = Settings()
